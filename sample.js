14689ca5ab08ea328a408e6762e13126fe42f7da
EC2 Default User <sigurokaru@gmail.com>
2023-02-07 15:31:23 +0000

    3

--- sample.js
+++ sample.js
@@ -1,4 +1,9 @@
-var nickname = "taro"
-console.log(nickname)
-nickname = "jiro"
-console.log(nickname)
+var str = "webcamp" 
+
+function foo() {
+  console.log(str)
+  var y = "hello" 
+}
+
+foo()
+console.log(y)
