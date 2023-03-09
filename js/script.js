c53c85ddc04f3f8aa8abdfa34e977226c42f2ac4
EC2 Default User <sigurokaru@gmail.com>
2023-03-09 05:56:30 +0000

    11

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
-const API_KEY = "取得したAPIキー";
-
-$(function(){
-  $('#btn').on('click', function() {
-    $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
-      dataType : 'jsonp',
-    }).done(function (data){
-      $('#place').text(data.name);
-      $('#temp_max').text(data.main.temp_max);
-      $('#temp_min').text(data.main.temp_min);
-      $('#humidity').text(data.main.humidity);
-      $('#speed').text(data.wind.speed);
-      $('#weather').text(data.weather[0].main);
-      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-      $('img').attr("alt",data.weather[0].main);
-    }).fail(function (data) {
-      alert('通信に失敗しました。');      });
-  });
+const swiper = new Swiper('.swiper', {
+  loop: true,
+ 
+  pagination: { 
+    el: '.swiper-pagination', 
+  },
+ 
+  navigation: { 
+    nextEl: '.swiper-button-next', 
+    prevEl: '.swiper-button-prev', 
+  }
 });
