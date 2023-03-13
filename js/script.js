28364e7ea0f5c5412fe39351ea41be12b5e9f07f
EC2 Default User <sigurokaru@gmail.com>
2023-03-13 08:18:43 +0000

    12

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
- 
-  pagination: { 
-    el: '.swiper-pagination', 
-  },
- 
-  navigation: { 
-    nextEl: '.swiper-button-next', 
-    prevEl: '.swiper-button-prev', 
-  }
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
 });
