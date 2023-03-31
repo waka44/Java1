876f64ea0c2f31a830ea4646d2d8927e68f00268
EC2 Default User <sigurokaru@gmail.com>
2023-03-31 04:52:13 +0000

    16

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,13 @@
+:
+:
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "script.js"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+:
